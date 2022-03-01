2d809eec422a53ac9f9183cf813b85a322912d6d
guilhermeEloi <gui.eloi.silva@gmail.com>
2022-03-01 15:15:15 -0300

    npm install

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { RoundsModule } from './rounds/rounds.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
+import { RoundsModule } from './rounds/rounds.module';
 
 @Module({
-import { TypeOrmModule } from '@nestjs/typeorm';
-@Module({
   imports: [
-    ConfigModule.forRoot({isGlobal: true}),
+    ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot({
-      type:'postgres',
+      type: 'postgres',
       host: process.env.POSTGRES_HOST,
       port: parseInt(<string>process.env.POSTGRES_PORT),
       username: process.env.POSTGRES_USER,
@@ -19,7 +18,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       synchronize: true,
     }),
     UsersModule,
-    RoundsModule
+    RoundsModule,
   ],
   controllers: [],
   providers: [],
