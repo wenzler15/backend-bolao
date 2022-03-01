f3eeffa7466755c385554c5bdce81f640bc6eba9
guilhermeEloi <gui.eloi.silva@gmail.com>
2022-03-01 15:04:23 -0300

    crud rounds

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { RoundsModule } from './rounds/rounds.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/bolao'), UsersModule],
+  imports: [MongooseModule.forRoot('mongodb://localhost/bolao'), UsersModule, RoundsModule],
   controllers: [],
   providers: [],
 })
