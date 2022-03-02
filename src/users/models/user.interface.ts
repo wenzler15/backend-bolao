955e14f128b91e918a5914ffc8e85e2cf512c9b7
Douglas <douglas.wenzler@mobitype.com.br>
2022-03-02 13:20:55 -0300

    Finished

--- src/users/models/user.interface.ts
+++ src/users/models/user.interface.ts
@@ -2,9 +2,10 @@ export interface User {
     id?: number;
     email?: string;
     name?: string;
-    favoriteTeam?: string;
+    favoriteTeam?: number;
     password?: string;
     createdAt?: Date;
+    facebookToken?: string;
     passwordResetToken?: string;
     passwordResetExpires?: Date;
 }
